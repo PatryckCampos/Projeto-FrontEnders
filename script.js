b983bb428bc5183f19f44f93cdde2b20d2749f0a
thiagosauro <thsilva.developer@gmail.com>
2022-11-11 16:08:47 -0300

    (v1.2): adicionei DOM (aperte T para aumentar tamanho de textos); aprimorei css login/cadastro (height > min-height); melhorei comentários gerais; etc.

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Manipulações DOM */
+
+function handleClickT(event){
+    console.log(event.key);
+    if(event.key === ('t')){
+        document.documentElement.classList.toggle('textomaior');
+    }
+}
+
+window.addEventListener('keydown', handleClickT);
